ceda4bc0e2db1b1b27d946b3ecfd2b7f4c3e5207
flikkes <lpke.flx@gmail.com>
2020-12-16 00:21:27 +0100

    finished basic calculation model and gui

--- app.py
+++ app.py
@@ -3,12 +3,30 @@ import tkinter
 import os
 
 
-top = tkinter.Tk()
-top.geometry("800x600")
+bearing_joint_val = 1.2
+butt_joint_val = 0.8
 
-Label(top, text="Steinanzahl").pack()
-stone_count = Entry(top)
-stone_count.pack()
+top = tkinter.Tk()
+top.geometry("500x800")
+
+wall_area_val = tkinter.DoubleVar(top)
+stone_count_height_val = tkinter.DoubleVar(top)
+stone_count_length_val = tkinter.DoubleVar(top)
+stones_needed_val = tkinter.DoubleVar(top)
+mortar_needed_bearing_joint = tkinter.DoubleVar(top)
+mortar_needed_butt_joint = tkinter.DoubleVar(top)
+mortar_needed_overall = tkinter.DoubleVar(top)
+
+
+def calculate():
+    wall_area_val.set((int(wall_height.get())*int(wall_length.get()))/10000)
+    stone_count_height_val.set((int(wall_height.get())+bearing_joint_val)/int(stone_height.get()))
+    stone_count_length_val.set(int(wall_length.get())/(int(stone_length.get())+butt_joint_val))
+    stones_needed_val.set(stone_count_height_val.get()*stone_count_length_val.get())
+    mortar_needed_bearing_joint.set((stone_count_height_val.get()*int(wall_length.get())*bearing_joint_val*int(stone_width.get()))/1000000)
+    mortar_needed_butt_joint.set((int(stone_height.get())*int(stone_width.get())*butt_joint_val*stones_needed_val.get())/1000000)
+    mortar_needed_overall.set(mortar_needed_bearing_joint.get()+mortar_needed_butt_joint.get())
+
 
 Label(top, text="Steinhöhe").pack()
 stone_height = Entry(top)
@@ -22,23 +40,46 @@ Label(top, text="Steinbreite").pack()
 stone_width = Entry(top)
 stone_width.pack()
 
-label_bearing_joint = Label(top, text="Lagerfuge: 1,2 cm")
-label_bearing_joint.pack()
+Label(top, text="Lagerfuge in cm: ").pack()
+Label(top, text=bearing_joint_val).pack()
 
-label_butt_joint = Label(top, text="Stoßfuge: 0,8 cm")
-label_butt_joint.pack()
+Label(top, text="Stoßfuge in cm: ").pack()
+Label(top, text=butt_joint_val).pack()
 
-Label(top, text="Mauerhöhe").pack()
+Label(top, text="Mauerhöhe in cm").pack()
 wall_height = Entry(top)
 wall_height.pack()
 
-Label(top, text="Mauerlänge").pack()
+Label(top, text="Mauerlänge in cm").pack()
 wall_length = Entry(top)
 wall_length.pack()
 
-Label(top, text="Mauerbreite").pack()
+Label(top, text="Mauerbreite in Steinen").pack()
 wall_width = Entry(top)
 wall_width.pack()
+
+Label(top, text="Mauerfläche in m²: ").pack()
+Label(top, textvariable=wall_area_val).pack()
+
+Label(top, text="Steinanzahl Mauerhöhe: ").pack()
+Label(top, textvariable=stone_count_height_val).pack()
+
+Label(top, text="Steinanzahl Mauerlänge: ").pack()
+Label(top, textvariable=stone_count_length_val).pack()
+
+Label(top, text="Steinbedarf: ").pack()
+Label(top, textvariable=stones_needed_val).pack()
+
+Label(top, text="Mörtelbedarf lagerfuge in m³: ").pack()
+Label(top, textvariable=mortar_needed_bearing_joint).pack()
+
+Label(top, text="Mörtelbedarf Stoßfuge in m³: ").pack()
+Label(top, textvariable=mortar_needed_butt_joint).pack()
+
+Label(top, text="Mörtelbedarf gesamt in m³: ").pack()
+Label(top, textvariable=mortar_needed_overall).pack()
+
+Button(top, text= "Berechnen", command=calculate).pack()
 
 
 top.mainloop()
